5a9be8eb40c70f9d9530e56823553e538f83832c
TengYingWei <734970600@qq.com>
2022-02-09 11:20:31 +0800

    Change logo

--- src/modules/header/ui/navigation/Navigation.tsx
+++ src/modules/header/ui/navigation/Navigation.tsx
@@ -1,9 +1,7 @@
 import type { FC } from "react";
 import styles from "./Navigation.module.scss";
 import { MaybeWithClassName } from "../../../../helper/react/types";
-import { NavLink } from "../../../../ui/button";
-import { SOCIAL } from "../../../../const/const";
-import { Envelop, GitHub, Medium, Telegram, Twitter } from "../../../../ui/icons/Icons";
+import { Envelop, Medium, Telegram, Twitter } from "../../../../ui/icons/Icons";
 
 export type LinkType = {
 	link: string;
@@ -17,7 +15,6 @@ type NavigationType = {
 type ComponentType = NavigationType & MaybeWithClassName;
 
 const ICONS = {
-	Github: <GitHub />,
 	Twitter: <Twitter />,
 	Telegram: <Telegram />,
 	Medium: <Medium />,
