57e9d4e0e221c082985e091f931503d651be5b79
TengYingWei <734970600@qq.com>
2022-04-01 08:18:57 +0800

    Improve

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { pageWithLayout } from "../src/utils/page/pageInLayout";
 import { Layout } from "../src/layout";
 import { Home } from "../src/pages/home";
-import { Sonet } from "../src/pages/sonet";
 
 const Index = pageWithLayout(
 	() => <Home />,
